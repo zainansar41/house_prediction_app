cd92333b2dd2f69dffa2b7c19c0c12a40157073d
ZAIN UL ABIDIN <zainansarp41@gmail.com>
2023-11-09 13:22:06 +0500

    working on server and frontend

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './home.css';
+
+export default function Home() {
+    return (
+      <div className="home-container">
+        <div className="background-image"></div>
+        <div className="content-container">
+          <div className="card">
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
